a4ab9da34e37dc624b157ceb5d06edad9e6089e8
Illia666 <108007460+Illia666@users.noreply.github.com>
2023-04-07 11:39:45 +0300

    update

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import * as React from "react";
-// import { useState } from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
 
 import { useRoute } from "./router";
-// import react from "react";
 
 
 export default function App() {
-  const routing = useRoute({});
+  const routing = useRoute(false);
   return <NavigationContainer>{routing}</NavigationContainer>;
 }
 
